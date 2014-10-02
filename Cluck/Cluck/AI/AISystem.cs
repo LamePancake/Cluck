75257b835cc6d91c40c30a2d224fd3a60c1ea406
funintentions <danr94@gmail.com>
2014-10-02 02:16:25 +0000

    Added component & system classes as well as some AI testing

---
+++ Cluck/Cluck/AI/AISystem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Cluck.AI
+{
+    class AISystem : GameSystem
+    {
+
+        public AISystem(Game game) : base(game, (int)component_flags.aiThinking)
+        {
+
+        }
+
+        public void Update(List<GameEntity> world)
+        {
+            foreach (GameEntity entity in world)
+            {
+                if (entity.HasComponent(myFlag))
+                {
+                    Console.WriteLine("YUP");
+                }
+            }
+        }
+
+        public void UpdateWorld(List<GameEntity> world)
+        {
+            foreach (GameEntity entity in world)
+            {
+                if (entity.HasComponent(myFlag))
+                {
+                    Console.WriteLine("YUP");
+                }
+            }
+        }
+    }
+}
---
+++ Cluck/Cluck/GameSystem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Cluck
+{
+    class GameSystem : GameComponent
+    {
+        protected int myFlag;
+
+        public GameSystem(Game game, int flag) : base(game)
+        {
+            myFlag = flag;
+        }
+    }
+}
