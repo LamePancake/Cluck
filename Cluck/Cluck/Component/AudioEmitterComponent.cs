d2c4d06e1f04b2a7e5b1abe36ba6ff30148ef335
twarrre <tw4194@hotmail.com>
2014-10-27 01:57:58 +0000

    Added the audio components again

---
+++ Cluck/Cluck/Component/AudioEmitterComponent.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+
+namespace Cluck
+{
+    class AudioEmitterComponent : Component
+    {
+        private AudioEmitter _emitter;
+        private SoundEffectInstance prevSound;
+
+        public AudioEmitterComponent(SoundEffectInstance s)
+            : base((int)component_flags.audioEmitter)
+        {
+            _emitter = new AudioEmitter();
+            prevSound = s;
+        }
+
+        public void SetPosition(Vector3 pos)
+        {
+            _emitter.Position = pos;
+        }
+
+        public AudioEmitter GetEmitter()
+        {
+            return _emitter;
+        }
+
+        public Vector3 GetPostion()
+        {
+            return _emitter.Position;
+        }
+
+        public SoundEffectInstance GetPrevSound()
+        {
+            return prevSound;
+        }
+
+        public void SetPrevSound(SoundEffectInstance s)
+        {
+            prevSound = s;
+        }
+
+        public void SetVelocity(Vector3 vel)
+        {
+            _emitter.Velocity = vel;
+        }
+
+        public Vector3 GetVelocity()
+        {
+            return _emitter.Velocity;
+        }
+    }
+}
+
---
+++ Cluck/Cluck/Component/AudioListenerComponent.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+
+namespace Cluck
+{
+    class AudioListenerComponent : Component
+    {
+        private AudioListener _listener;
+
+        public AudioListenerComponent()
+            : base((int)component_flags.audioListener)
+        {
+            _listener = new AudioListener();
+        }
+
+        public void SetPosition(Vector3 pos)
+        {
+            _listener.Position = pos;
+        }
+
+        public AudioListener GetListener()
+        {
+            return _listener;
+        }
+
+        public Vector3 GetPostion()
+        {
+            return _listener.Position;
+        }
+
+        public void SetVelocity(Vector3 vel)
+        {
+            _listener.Velocity = vel;
+        }
+
+        public Vector3 GetVelocity()
+        {
+            return _listener.Velocity;
+        }
+    }
+}
+
